c1804915d593b3640efceccf9045bd9d031a417e
kennethmarkhui <kennethmarkhui@gmail.com>
2022-10-25 12:30:36 +0800

    revert changes to TransparentCard background image and use tailwind to handle image opacity

--- src/components/cards/TransparentCard.tsx
+++ src/components/cards/TransparentCard.tsx
@@ -13,18 +13,14 @@ const TransparentCard = ({
     button,
 }: ITransparentCard): JSX.Element => {
     return (
-        <div>
-            <div className="bg-[url('assets/image/coffee_and_beans.jpg')] max-w-screen-xl h-80 bg-cover bg-center  mx-auto my-10">
-                <div className="py-10 flex flex-col items-center">
-                    <div className="flex justify-center text-4xl py-3">
-                        {title}
-                    </div>
-                    <img className="w-72 h-10 flex" src={divider}></img>
-                    <div className="flex justify-center text-2xl py-4">
-                        {description}
-                    </div>
-                    <Button>{button}</Button>
+        <div className="bg-[url('assets/image/coffee_and_beans.jpg')] max-w-screen-xl h-80 bg-neutral-900 bg-cover bg-no-repeat bg-blend-multiply bg-center mx-auto my-10">
+            <div className="py-10 flex flex-col items-center">
+                <div className="flex justify-center text-4xl py-3">{title}</div>
+                <img className="w-72 h-10 flex" src={divider}></img>
+                <div className="flex justify-center text-2xl py-4">
+                    {description}
                 </div>
+                <Button>{button}</Button>
             </div>
         </div>
     )
