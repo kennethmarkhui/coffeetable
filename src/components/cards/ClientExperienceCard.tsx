eb3506e83b5a1ab2ad0ee6f9636df6ccb5e2aa08
Wisely <wiselychung96@gmail.com>
2022-10-27 21:22:44 +0800

    fixed some minor css

--- src/components/cards/ClientExperienceCard.tsx
+++ src/components/cards/ClientExperienceCard.tsx
@@ -13,11 +13,13 @@ const CustomerExperienceCard = ({
 }: IClientExperienceCard): JSX.Element => {
     return (
         <div className="max-w-full h-[46rem] p-8 shadow-md border-none bg-black flex flex-col mx-auto gap-12 sm:flex-row">
-            <div className="basis-1/2 p-4">
-                <div className="text-yellow0">{title}</div>
+            <div className="basis-1/2 p-4 flex justify-center flex-col">
+                <div className="text-yellow0 text-4xl">{title}</div>
                 <div className="">{subject}</div>
-                <img className="w-72 h-10 flex" src={divider}></img>
-                <div className="">{testimonies}</div>
+                <img className="w-72 h-10 flex mx-auto" src={divider}></img>
+                <div className="mx-auto p-8">
+                    <div className="">{testimonies}</div>
+                </div>
             </div>
             <div className="basis-1/2 p-4">
                 <img src="http://placekitten.com/1200/650"></img>
