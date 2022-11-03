6f6b01bd79b4053b8975a069e695a080dcbbf8a0
kennethmarkhui <kennethmarkhui@gmail.com>
2022-11-03 13:54:35 +0800

    added a map using react-leaflet

--- src/components/WhereAreWe.tsx
+++ src/components/WhereAreWe.tsx
@@ -1,4 +1,14 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+import 'leaflet/dist/leaflet.css'
+
 import divider from '../assets/image/divider.png'
+
+type Coordinate = [number, number]
+
+const coordinates: Coordinate[] = [
+    [51.505, -0.09],
+    [51.505, -0.09],
+]
 
 const WhereAreWe = (): JSX.Element => {
     return (
@@ -16,9 +26,24 @@ const WhereAreWe = (): JSX.Element => {
                     aperiam commodi fugiat doloremque consectetur. Repudiandae,
                     natus voluptates officia quisquam ex itaque sapiente?
                 </p>
-                <div className="grid md:grid-cols-2 gap-8 mx-auto">
-                    <img src="http://placekitten.com/500/400" alt="" />
-                    <img src="http://placekitten.com/500/400" alt="" />
+                <div className="grid md:grid-cols-2 gap-8 mx-auto z-0">
+                    {coordinates.map((coordinate, index) => (
+                        <MapContainer
+                            key={Math.random() + index}
+                            center={coordinate}
+                            zoom={13}
+                            scrollWheelZoom={false}
+                            className="w-72 h-72 sm:w-96 sm:h-96"
+                        >
+                            <TileLayer
+                                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                            />
+                            <Marker position={coordinate}>
+                                <Popup>You can find us here.</Popup>
+                            </Marker>
+                        </MapContainer>
+                    ))}
                 </div>
             </div>
         </section>
