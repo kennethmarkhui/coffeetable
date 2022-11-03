4f401affe0087cacb10869e74a3ff8ffa18f8c34
kennethmarkhui <kennethmarkhui@gmail.com>
2022-11-03 17:23:15 +0800

    responsive OurStory page

--- src/pages/OurStory.tsx
+++ src/pages/OurStory.tsx
@@ -22,17 +22,17 @@ const OurStory = (): JSX.Element => {
     return (
         <>
             <Hero subject={ourStoryHeroContent.subject} />
-            <div className="bg-black max-w-screen-xl mx-auto my-6">
-                <div className="grid grid-cols-4 gap-4">
-                    <img
-                        src="http://placekitten.com/550/550"
-                        alt=""
-                        className="col-span-2 p-8"
-                    />
-                    <div>
-                        <div className="p-8 text-2xl">WHO ARE COFFEE TABLE</div>
-                        <img className="w-72 p-8" src={divider} />
-                        <div>
+            <section className="max-w-screen-xl mx-auto flex flex-col lg:flex-row gap-8 p-8">
+                <img
+                    src="http://placekitten.com/550/550"
+                    alt=""
+                    className="flex-1"
+                />
+                <div className="flex-1 flex flex-col lg:flex-row gap-8">
+                    <div className="space-y-4">
+                        <h1 className="text-2xl">WHO ARE COFFEE TABLE</h1>
+                        <img className="w-72" src={divider} />
+                        <p className="text-sm md:text-base">
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Quod atque possimus aspernatur amet, earum
                             labore commodi beatae itaque ipsum fugit velit
@@ -41,11 +41,11 @@ const OurStory = (): JSX.Element => {
                             nostrum nisi. Sunt ullam aperiam eveniet temporibus
                             nemo, minus suscipit doloremque officiis impedit ea
                             dolore.
-                        </div>
-                        <Button className="my-4">Book now</Button>
+                        </p>
+                        <Button>Book now</Button>
                     </div>
-                    <div>
-                        <div className="p-8">
+                    <div className="space-y-4">
+                        <p className="text-sm md:text-base">
                             Lorem ipsum dolor sit amet consectetur adipisicing
                             elit. Unde asperiores laborum doloremque perferendis
                             eos excepturi ratione ea dolore distinctio,
@@ -53,74 +53,61 @@ const OurStory = (): JSX.Element => {
                             ipsum temporibus dicta! Voluptates, accusamus vitae
                             possimus voluptatum aliquid iste id odit error et
                             fugit!
-                        </div>
+                        </p>
                         <img
                             src="http://placekitten.com/550/550"
                             alt=""
-                            className="col-span-1 p-8"
+                            className=""
                         />
                     </div>
                 </div>
-            </div>
-            <div className="bg-black max-w-screen-xl mx-auto">
-                <div className="flex flex-row ">
-                    <div className="basis-1/2 gap-4">
-                        <div className="flex flex-col p-10">
-                            <div className="text-4xl mt-6 m-4">
-                                Our Inspiration
-                            </div>
-                            <img className="w-72 m-4" src={divider} />
-                            <div className="m-4 p-4">
-                                Lorem ipsum dolor sit amet consectetur
-                                adipisicing elit. Labore neque sequi ab
-                                dignissimos adipisci quidem libero! Accusamus
-                                esse voluptate obcaecati repellendus beatae
-                                mollitia possimus numquam autem nobis? Iusto
-                                nihil, debitis nobis obcaecati eius quisquam,
-                                iure facilis omnis voluptatum est officia quis
-                                iste tempore ab sequi, accusantium eos cumque
-                                quod illo. Vitae totam quaerat eos ipsa quae
-                                quia ratione beatae laudantium!
-                            </div>
-                        </div>
-                    </div>
-                    <div className="basis-1/2">
+            </section>
+            <section className="bg-black max-w-screen-xl mx-auto p-8">
+                <div className="flex flex-col lg:flex-row gap-4">
+                    <div className="flex-1 lg:order-last">
                         <img
                             src="http://placekitten.com/550/550"
                             alt=""
-                            className="p-6 m-auto"
+                            className="m-auto"
                         />
                     </div>
-                </div>
-            </div>
-            <div className="bg-black max-w-screen-xl mx-auto my-6">
-                <div className="flex flex-row">
-                    <div className="basis-1/2 gap-4">
-                        <iframe
-                            className="m-auto p-4"
-                            width="560"
-                            height="315"
-                            src="https://www.youtube.com/embed/lWLxD7BzZ_Y"
-                            title="YouTube video player"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen
-                        ></iframe>
-                    </div>
-                    <div className="basis-1/2 gap-4">
-                        <iframe
-                            className="m-auto p-4"
-                            width="560"
-                            height="315"
-                            src="https://www.youtube.com/embed/n-ozFN8tGKM"
-                            title="YouTube video player"
-                            frameborder="0"
-                            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                            allowfullscreen
-                        ></iframe>
+                    <div className="flex-1 flex flex-col gap-4">
+                        <h1 className="text-2xl md:text-4xl">
+                            Our Inspiration
+                        </h1>
+                        <img className="w-72" src={divider} />
+                        <p className="text-sm md:text-base">
+                            Lorem ipsum dolor sit amet consectetur adipisicing
+                            elit. Labore neque sequi ab dignissimos adipisci
+                            quidem libero! Accusamus esse voluptate obcaecati
+                            repellendus beatae mollitia possimus numquam autem
+                            nobis? Iusto nihil, debitis nobis obcaecati eius
+                            quisquam, iure facilis omnis voluptatum est officia
+                            quis iste tempore ab sequi, accusantium eos cumque
+                            quod illo. Vitae totam quaerat eos ipsa quae quia
+                            ratione beatae laudantium!
+                        </p>
                     </div>
                 </div>
-            </div>
+            </section>
+            <section className="flex flex-col sm:flex-row max-w-screen-xl mx-auto gap-8 p-8">
+                <iframe
+                    className="w-full aspect-video"
+                    src="https://www.youtube.com/embed/lWLxD7BzZ_Y"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+                <iframe
+                    className="w-full aspect-video"
+                    src="https://www.youtube.com/embed/n-ozFN8tGKM"
+                    title="YouTube video player"
+                    frameBorder="0"
+                    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+                    allowFullScreen
+                ></iframe>
+            </section>
             <TransparentCard
                 title={ourStoryTransparentContent.title}
                 description={ourStoryTransparentContent.description}
