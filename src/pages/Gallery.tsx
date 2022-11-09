c8531bbe7d5f8e4b4945a64ab0222eb8325c3fc8
kennethmarkhui <kennethmarkhui@gmail.com>
2022-11-09 15:06:27 +0800

    fixed gallery lightbox

--- src/pages/Gallery.tsx
+++ src/pages/Gallery.tsx
@@ -1,9 +1,11 @@
+import { useState } from 'react'
+import FsLightbox from 'fslightbox-react'
+import { Portal } from '@headlessui/react'
+
 import Hero, { IHero } from '../components/Hero'
 import TransparentCard, {
     ITransparentCard,
 } from '../components/cards/TransparentCard'
-import React, { useState } from 'react'
-import FsLightbox from 'fslightbox-react'
 
 const galleryHeroContent: IHero = {
     subject: 'See Our Delightful Cafe in Action',
@@ -15,8 +17,35 @@ const galleryTransparentContent: ITransparentCard = {
     button: 'Contact Us',
 }
 
+interface IImage {
+    id: number
+    src: string
+}
+
+const images: IImage[] = [
+    { id: 1, src: 'http://placekitten.com/250/250' },
+    { id: 2, src: 'http://placekitten.com/250/260' },
+    { id: 3, src: 'http://placekitten.com/260/250' },
+    { id: 4, src: 'http://placekitten.com/270/250' },
+    { id: 5, src: 'http://placekitten.com/250/280' },
+    { id: 6, src: 'http://placekitten.com/300/250' },
+    { id: 7, src: 'http://placekitten.com/250/300' },
+    { id: 8, src: 'http://placekitten.com/240/440' },
+]
+
 const Gallery = (): JSX.Element => {
-    const [toggler, setToggler] = useState(false)
+    const [lightboxController, setLightboxController] = useState({
+        toggler: false,
+        slide: 1,
+    })
+
+    const openLightboxOnSlide = (number: number): void => {
+        setLightboxController({
+            toggler: !lightboxController.toggler,
+            slide: number,
+        })
+    }
+
     return (
         <>
             <Hero subject={galleryHeroContent.subject} />
@@ -26,48 +55,26 @@ const Gallery = (): JSX.Element => {
                     alt="brand"
                     className="mx-auto"
                 />
-                {/** 
-                 <div className="flex justify-center">
-                    <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-                        <img src="http://placekitten.com/250/250" alt="1" />
-                        <img src="http://placekitten.com/250/250" alt="2" />
-                        <img src="http://placekitten.com/250/250" alt="3" />
-                        <img src="http://placekitten.com/250/250" alt="4" />
-                        <img src="http://placekitten.com/250/250" alt="5" />
-                        <img src="http://placekitten.com/250/250" alt="6" />
-                        <img src="http://placekitten.com/250/250" alt="7" />
-                        <img src="http://placekitten.com/250/250" alt="8" />
-                    </div>
-                </div>
-                */}
 
-                <div className="flex justify-center">
-                    <button onClick={() => setToggler(!toggler)}>
-                        <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
-                            <img src="http://placekitten.com/250/250" alt="1" />
-                            <img src="http://placekitten.com/250/251" alt="2" />
-                            <img src="http://placekitten.com/250/252" alt="3" />
-                            <img src="http://placekitten.com/250/253" alt="4" />
-                            <img src="http://placekitten.com/250/254" alt="5" />
-                            <img src="http://placekitten.com/250/255" alt="6" />
-                            <img src="http://placekitten.com/250/256" alt="7" />
-                            <img src="http://placekitten.com/250/257" alt="8" />
+                <div className="grid grid-cols-2 lg:grid-cols-4 gap-4">
+                    {images.map((image) => (
+                        <div key={image.id} className="w-full h-36 sm:h-48">
+                            <img
+                                className="object-cover object-center w-full h-full"
+                                onClick={() => openLightboxOnSlide(image.id)}
+                                src={image.src}
+                                alt={`image-${image.id}`}
+                            />
                         </div>
-                    </button>
+                    ))}
                 </div>
-                <FsLightbox
-                    toggler={toggler}
-                    sources={[
-                        'http://placekitten.com/250/250',
-                        'http://placekitten.com/250/251',
-                        'http://placekitten.com/250/252',
-                        'http://placekitten.com/250/253',
-                        'http://placekitten.com/250/254',
-                        'http://placekitten.com/250/255',
-                        'http://placekitten.com/250/256',
-                        'http://placekitten.com/250/257',
-                    ]}
-                />
+                <Portal>
+                    <FsLightbox
+                        toggler={lightboxController.toggler}
+                        sources={images.map((image) => image.src)}
+                        slide={lightboxController.slide}
+                    />
+                </Portal>
             </section>
 
             <TransparentCard
