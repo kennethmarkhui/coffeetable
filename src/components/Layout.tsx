94755775940fd80b1889ac605c8f2fa07ac30a31
kennethmarkhui <kennethmarkhui@gmail.com>
2022-11-15 19:16:51 +0800

    scroll to top when changing page route

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,9 +1,16 @@
-import { Outlet } from 'react-router-dom'
+import { useEffect } from 'react'
+import { Outlet, useLocation } from 'react-router-dom'
 
 import Header from './Header'
 import Footer from './Footer'
 
 const Layout = (): JSX.Element => {
+    const { pathname } = useLocation()
+
+    useEffect(() => {
+        document.documentElement.scrollTo(0, 0)
+    }, [pathname])
+
     return (
         <>
             <Header />
